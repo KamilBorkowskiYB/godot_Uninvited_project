b8ee0a2650fcba5a8310d6d1d32fb97ef44fdeaa
Kamil <kamil.borkow@wp.pl>
2024-03-17 15:29:52 +0100

    Added legs and animeted them

--- player.gd
+++ player.gd
@@ -5,12 +5,15 @@ extends CharacterBody2D
 @export var fog: Node2D #viewPorty
 
 @onready var ray_cast_2d = $RayCast2D
-@onready var animation_player = $AnimationPlayer
+@onready var animation_player = $AnimationPlayerTOP
+@onready var animation_playerLegs = $AnimationPlayerLEGS
 @export var move_speed = 200
 var dead = false
+var target_angle = 0
 @export var can_shoot = false
 func _ready():
 	can_shoot = false
+	
 func _process(_delta):
 	if camera != null:
 		camera.position = self.position
@@ -27,7 +30,17 @@ func _process(_delta):
 		return
 	
 	global_rotation = global_position.direction_to(get_global_mouse_position()).angle() + PI/2
+	#$Graphics/Alive.rotation = global_position.direction_to(get_global_mouse_position()).angle() + PI/2
+	var move_direction = Input.get_vector("move_left","move_right","move_down","move_up")
+	$Graphics/Legs.rotation = target_angle
 	
+	if move_direction.length_squared() > 0:
+		target_angle = move_direction.angle_to(Vector2(1, 0)) - PI/2
+	
+	if velocity.length() > 0:
+		animation_playerLegs.play("walk")
+	else:
+		animation_playerLegs.stop()
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 	
--- zombie.gd
+++ zombie.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 @export var move_speed = 100
 @onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
-
+@onready var animation_player = $AnimationPlayer
 var dead = false
 
 func _physics_process(_delta):
@@ -13,6 +13,7 @@ func _physics_process(_delta):
 	var dir_to_player = global_position.direction_to(player.global_position)
 	velocity = move_speed * dir_to_player
 	move_and_slide()
+	animation_player.play("walk")
 	
 	global_rotation = dir_to_player.angle() + PI/2.0
 	
@@ -26,6 +27,7 @@ func kill():
 	$DeathSound.play()
 	$Graphics/Dead.show()
 	$Graphics/Alive.hide()
+	$Graphics/Legs.hide()
 	$CollisionShape2D.disabled = true
 	z_index = -1
 	
