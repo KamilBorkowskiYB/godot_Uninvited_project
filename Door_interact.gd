b8a660607cd4473de692c9d4cb7246f4fdcc00f3
Kamil <kamil.borkow@wp.pl>
2024-04-01 21:19:37 +0200

    Added closing/opening doors

--- Door_interact.gd
+++ Door_interact.gd
@@ -2,8 +2,16 @@ extends RigidBody2D
 
 @onready var interaction_area: InteractionArea = $interaction_area
 var transform_to
+var isClosed = true
 func _ready():
 	interaction_area.interact = Callable(self,"_on_interact")
 	transform_to = self.transform
 func _on_interact():
-	self.transform = transform_to
+	if(isClosed):
+		isClosed = false
+		$".".mass = 1.0
+		apply_central_impulse(Vector2(0,-300000))
+	else:
+		self.transform = transform_to
+		isClosed = true
+		$".".mass = 1000.0
