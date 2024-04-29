4504f00fc7f09d62a915f9b622ed9a432130da40
Kamil <kamil.borkow@wp.pl>
2024-04-29 17:56:36 +0200

    updating files before updating godot to 4.2.2

--- Door_interact.gd
+++ Door_interact.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 @onready var interaction_area: InteractionArea = $interaction_area
 var transform_to
 var isClosed = true
+
 func _ready():
 	interaction_area.interact = Callable(self,"_on_interact")
 	transform_to = self.transform
@@ -12,6 +13,9 @@ func _on_interact():
 		$".".mass = 1.0
 		apply_central_impulse(Vector2(0,-300000))
 	else:
-		self.transform = transform_to
 		isClosed = true
+		$".".set_linear_velocity(Vector2.ZERO)
+		$".".set_angular_velocity(0)
+		await get_tree().create_timer(0.01).timeout
+		self.transform = transform_to
 		$".".mass = 1000.0
--- interaction_area.gd
+++ interaction_area.gd
@@ -2,14 +2,15 @@ extends Area2D
 class_name InteractionArea
 
 @export var action_name: String = "Open/Close"
-
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
 var interact: Callable = func():
 	pass
 
 
 func _on_body_entered(_body):
-	InteractionManager.register_area(self)
-
+	if _body == player:
+		InteractionManager.register_area(self)
 
 func _on_body_exited(_body):
-	InteractionManager.unregister_area(self)
+	if _body == player:
+		InteractionManager.unregister_area(self)
