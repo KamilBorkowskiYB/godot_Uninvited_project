d9819b316ff56203a62ff4b7b067b64f1938794a
Kamil <kamil.borkow@wp.pl>
2024-08-11 18:43:49 +0200

    reloading weapons

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -19,11 +19,11 @@ var stands_on := FloorMaterial.Concrete #floor changes this on ready()
 
 ##########        PICK UPS        ##########
 var pistol_unlock = 1
-var pistol_ammo = 20
+var pistol_ammo = 8
 var rifle_unlock = 1 #remember to update pickUp.gd with every new pick up
-var rifle_ammo = 5
+var rifle_ammo = 3
 var shotgun_unlock = 0
-var shotgun_shells = 0
+var shotgun_shells = 1
 
 ##########        PLAYER NODES        ##########
 @onready var ray_cast1 = $Top/RayCasts/RayCast2D
@@ -38,7 +38,6 @@ var shotgun_shells = 0
 @export var move_speed = 200
 @export var push_force = 20.0
 var dead = false
-var leg_direction_angle = 0
 var weapon_selected  #0-rifle	1-shotgun 	2-pistol
 @export var can_shoot = false
 @export var can_interact = true #not used at the moment
@@ -49,25 +48,34 @@ const PISTOL_MAX_RECOIL = 20.0
 const PISTOL_MIN_RECOIL = 6.0
 const PISTOL_FOCUS_SPEED = 0.2
 const PISTOL_DMG = 10.0
+const PISTOL_MAG_SIZE = 8
+const PISTOL_MAX_SIZE = 24
+var pistol_cur_mag = 8
 const PISTOL_AIM_ANIM = "aim_pistol"
 const PISTOL_AIMED_ANIM = "aimed_pistol"
-const PISTOL_MUZZLE_FLASH = Vector2(-1,-86)
+const PISTOL_RELOAD_ANIM = "reload_pistol"
 #rifle
 const RIFLE_MAX_RECOIL = 15.0
 const RIFLE_MIN_RECOIL = 3.0
 const RIFLE_FOCUS_SPEED = 0.1
 const RIFLE_DMG = 30.0
-const RIFLE_AIM_ANIM = "aim"
+const RIFLE_MAG_SIZE = 1
+const RIFLE_MAX_SIZE = 10
+var rifle_cur_mag = 1
+const RIFLE_AIM_ANIM = "aim_rifle"
 const RIFLE_AIMED_ANIM = "aimed_rifle"
-const RIFLE_MUZZLE_FLASH = Vector2(3,-89)
+const RIFLE_RELOAD_ANIM = "reload_rifle"
 #shotgun
 const SHOTGUN_MAX_RECOIL = 40.0
 const SHOTGUN_MIN_RECOIL = 10.0
 const SHOTGUN_FOCUS_SPEED = 0.4
 const SHOTGUN_DMG = 15
+const SHOTGUN_MAG_SIZE = 2
+const SHOTGUN_MAX_SIZE = 10
+var shotgun_cur_mag = 2
 const SHOTGUN_AIM_ANIM = "aim_shotgun"
 const SHOTGUN_AIMED_ANIM = "aimed_shotgun"
-const SHOTGUN_MUZZLE_FLASH = Vector2(1,-81)
+const SHOTGUN_RELOAD_ANIM = "reload_shotgun"
 #defalut
 var max_recoil = RIFLE_MAX_RECOIL
 var min_recoil = RIFLE_MIN_RECOIL#dynamic min recoil based of walking, etc.
@@ -77,12 +85,7 @@ var floor_min_recoil = RIFLE_MIN_RECOIL #static min recoil based of weapon
 var damage = RIFLE_DMG
 var animation_aim = RIFLE_AIM_ANIM
 var animation_aimed = RIFLE_AIMED_ANIM
-
-##########        CALCULATIN RAYCAST ROTATION        ##########
-var recoil_deg = randf_range(-recoil, recoil)
-var recoil_deg2 = randf_range(-recoil, recoil)
-var recoil_deg3 = randf_range(-recoil, recoil)
-var recoil_deg4 = randf_range(-recoil, recoil)
+var animation_reload = RIFLE_RELOAD_ANIM
 
 func _ready():
 	can_shoot = false
@@ -110,11 +113,10 @@ func _process(_delta):
 	##########        TOP AND LEGS ROTATION         ##########
 	$Top.rotation = global_position.direction_to(get_global_mouse_position()).angle() + PI/2
 	var move_direction = Input.get_vector("move_left","move_right","move_down","move_up")
-	$Legs.rotation = leg_direction_angle
-	
+	$Legs.rotation = move_direction.angle() + PI/2
+
 	if velocity.length() > 0:
-		animation_playerLegs.play("walk")
-		leg_direction_angle = move_direction.angle() + PI/2
+		animation_playerLegs.play("walking")
 		min_recoil = floor_min_recoil * 2
 		if(recoil < min_recoil):
 			recoil = min_recoil
@@ -125,27 +127,27 @@ func _process(_delta):
 		
 	##########        INPUTS         ##########
 	if Input.is_action_just_pressed("shoot"):
-		if weapon_selected == 0 and rifle_ammo > 0:
-			rifle_ammo = shoot([ray_cast1], rifle_ammo)
-		elif weapon_selected == 1 and shotgun_shells > 0:
-			shotgun_shells = shoot([ray_cast1,ray_cast2,ray_cast3,ray_cast4], shotgun_shells)
-		elif weapon_selected == 2 and pistol_ammo > 0:
-			pistol_ammo = shoot([ray_cast1], pistol_ammo)
+		if weapon_selected == 0 and rifle_cur_mag > 0:
+			rifle_cur_mag = shoot([ray_cast1], rifle_cur_mag)
+		elif weapon_selected == 1 and shotgun_cur_mag > 0:
+			shotgun_cur_mag = shoot([ray_cast1,ray_cast2,ray_cast3,ray_cast4], shotgun_cur_mag)
+		elif weapon_selected == 2 and pistol_cur_mag > 0:
+			pistol_cur_mag = shoot([ray_cast1], pistol_cur_mag)
 	
 	if Input.is_action_just_pressed("weapon_1"):
 		if rifle_unlock > 0:
 			change_weapon(39,0,RIFLE_MAX_RECOIL,RIFLE_MIN_RECOIL,
-			RIFLE_DMG,RIFLE_FOCUS_SPEED,RIFLE_AIM_ANIM,RIFLE_AIMED_ANIM,RIFLE_MUZZLE_FLASH)
+			RIFLE_DMG,RIFLE_FOCUS_SPEED,RIFLE_AIM_ANIM,RIFLE_AIMED_ANIM,RIFLE_RELOAD_ANIM)
 	
 	if Input.is_action_just_pressed("weapon_2"):
 		if shotgun_unlock > 0:
 			change_weapon(52,1,SHOTGUN_MAX_RECOIL,SHOTGUN_MIN_RECOIL,
-			SHOTGUN_DMG,SHOTGUN_FOCUS_SPEED,SHOTGUN_AIM_ANIM,SHOTGUN_AIMED_ANIM,SHOTGUN_MUZZLE_FLASH)
+			SHOTGUN_DMG,SHOTGUN_FOCUS_SPEED,SHOTGUN_AIM_ANIM,SHOTGUN_AIMED_ANIM,SHOTGUN_RELOAD_ANIM)
 	
 	if Input.is_action_just_pressed("weapon_3"):
 		if pistol_unlock > 0:
 			change_weapon(13,2,PISTOL_MAX_RECOIL,PISTOL_MIN_RECOIL,
-			PISTOL_DMG,PISTOL_FOCUS_SPEED,PISTOL_AIM_ANIM,PISTOL_AIMED_ANIM,PISTOL_MUZZLE_FLASH)
+			PISTOL_DMG,PISTOL_FOCUS_SPEED,PISTOL_AIM_ANIM,PISTOL_AIMED_ANIM,PISTOL_RELOAD_ANIM)
 	
 	if Input.is_action_just_pressed("Aim"):
 		if weapon_selected == null:
@@ -167,25 +169,18 @@ func _process(_delta):
 	else:
 		recoil = min(recoil + 0.1,max_recoil)
 	if recoil != 0:
-		recoil_deg = randf_range(-recoil, recoil)
-		recoil_deg2 = randf_range(-recoil, recoil)
-		recoil_deg3 = randf_range(-recoil, recoil)
-		recoil_deg4 = randf_range(-recoil, recoil)
-	ray_cast1.rotation_degrees = recoil_deg
-	ray_cast2.rotation_degrees = recoil_deg2
-	ray_cast3.rotation_degrees = recoil_deg3
-	ray_cast4.rotation_degrees = recoil_deg4
+		ray_cast1.rotation_degrees = randf_range(-recoil, recoil)
+		ray_cast2.rotation_degrees= randf_range(-recoil, recoil)
+		ray_cast3.rotation_degrees= randf_range(-recoil, recoil)
+		ray_cast4.rotation_degrees = randf_range(-recoil, recoil)
 	
 	if Input.is_action_just_released("Aim"):
-		if weapon_selected == null:
-			return
-		animation_player.play_backwards(animation_aim)
-		animation_playerLegs.speed_scale = 1
-		RIGHT_aim_assitst.hide()
-		LEFT_aim_assitst.hide()
-		Input.set_custom_mouse_cursor(cursor_normal,Input.CURSOR_ARROW,Vector2(24,24))
-		cursor_current = cursor_normal
-		move_speed = 200
+		unaim()
+	
+	if Input.is_action_just_pressed("reload"):
+		if weapon_selected != null:
+			unaim()
+			animation_player.queue(animation_reload)
 
 func _physics_process(_delta):
 	if dead:
@@ -243,7 +238,44 @@ func shoot(ray_casts,ammo_type):
 					ray_cast.get_collider().kill(attack)
 	return ammo_type
 
-func change_weapon(frame,wep_num,max_rec,min_rec,dmg,rec_sped,anim_aim,anim_aimed,muzzle_ofset):
+func unaim():
+	if weapon_selected == null:
+		return
+	if cursor_current == cursor_aim:
+		animation_player.play_backwards(animation_aim)
+		animation_playerLegs.speed_scale = 1
+		RIGHT_aim_assitst.hide()
+		LEFT_aim_assitst.hide()
+		Input.set_custom_mouse_cursor(cursor_normal,Input.CURSOR_ARROW,Vector2(24,24))
+		cursor_current = cursor_normal
+		move_speed = 200
+
+func animation_end_reload():
+	if weapon_selected == 0 and rifle_cur_mag < RIFLE_MAG_SIZE and rifle_ammo > 0:
+		var resoult = reload(rifle_cur_mag,RIFLE_MAG_SIZE,rifle_ammo)
+		rifle_cur_mag = resoult[0]
+		rifle_ammo = resoult[1]
+	elif weapon_selected == 1 and shotgun_cur_mag < SHOTGUN_MAG_SIZE and shotgun_shells > 0:
+		var resoult = reload(shotgun_cur_mag,SHOTGUN_MAG_SIZE,shotgun_shells)
+		shotgun_cur_mag = resoult[0]
+		shotgun_shells = resoult[1]
+	elif weapon_selected == 2 and pistol_cur_mag < PISTOL_MAG_SIZE and pistol_ammo > 0:
+		var resoult = reload(pistol_cur_mag,PISTOL_MAG_SIZE,pistol_ammo)
+		pistol_cur_mag = resoult[0]
+		pistol_ammo = resoult[1]
+
+func reload(curr_mag,mag_size,ammo):
+	var needed_ammo = mag_size - curr_mag
+	var ammo_taken
+	if needed_ammo <= ammo:
+		ammo_taken = needed_ammo
+	else:
+		ammo_taken = ammo
+	curr_mag = curr_mag + ammo_taken
+	ammo = ammo - ammo_taken
+	return [curr_mag, ammo]
+
+func change_weapon(frame,wep_num,max_rec,min_rec,dmg,rec_sped,anim_aim,anim_aimed,anim_rel):
 	if cursor_current != cursor_aim and !animation_player.is_playing():
 		$Top/Alive.frame = frame
 		max_recoil = max_rec
@@ -254,8 +286,8 @@ func change_weapon(frame,wep_num,max_rec,min_rec,dmg,rec_sped,anim_aim,anim_aime
 		floor_min_recoil = min_rec
 		animation_aim = anim_aim
 		animation_aimed = anim_aimed
+		animation_reload = anim_rel
 		recoil = max_rec * 0.7
-		$Top/MuzzleFlash.position = muzzle_ofset
 
 func step():
 	if stands_on == FloorMaterial.Concrete:
