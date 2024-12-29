3e878f7bc72afc874f5a99374fe19e42c918e5a4
Kamil <kamil.borkow@wp.pl>
2024-12-29 15:27:28 +0100

    Hidding transparent nodes on hover and close to player

--- levels/camera_control.gd
+++ levels/camera_control.gd
@@ -5,6 +5,19 @@ var max_view_distance = 600
 var shake_amount = 0
 var shake_timer = 0
 
+var ListOfMaterials : Array = []
+var PositionArray : Array[Vector2] = [Vector2(0,0),Vector2(0,0)]
+const MaxPositions : int = 2
+
+func _ready():
+	PositionArray.resize(MaxPositions)
+	var transparent_nodes : Array = get_tree().get_nodes_in_group( "Transparent" )
+	for n in transparent_nodes :
+		var mat : ShaderMaterial = n.material
+		if not mat == null :
+			ListOfMaterials.append( mat )
+
+
 func _process(_delta):
 	var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
 	if player == null:
@@ -13,11 +26,13 @@ func _process(_delta):
 	var player_position = player.global_position
 	var distance = player_position.distance_to(mouse_position)
 	var target_position = player_position
+	var screen_position = Vector2(960,540)
 
 	if distance > DEAD_ZONE:
 		var direction = (mouse_position - player_position).normalized()
 		var clamped_distance = min(distance, max_view_distance)
 		target_position = player_position + direction * (clamped_distance - DEAD_ZONE) * 0.5
+		screen_position = Vector2(960,540)- direction * (clamped_distance - DEAD_ZONE) * 0.5
 	
 	$PlayerCamera.global_position = target_position
 	
@@ -26,7 +41,23 @@ func _process(_delta):
 		$PlayerCamera.offset = Vector2(randi_range(-shake_amount, shake_amount), randi_range(-shake_amount, shake_amount))
 	else:
 		$PlayerCamera.offset = Vector2.ZERO
+	
+	var new_position = get_viewport().get_mouse_position()
+	PositionArray.insert(0, new_position)
+	PositionArray.insert(1, screen_position)
+	while PositionArray.size() > MaxPositions:
+		PositionArray.pop_back()
+	update_materials_with_positions()
 
 func start_shake(amount, duration):
 	shake_amount = amount
 	shake_timer = duration
+
+
+func update_materials_with_positions() -> void:
+	while PositionArray.size() > 2 :
+		PositionArray.erase( PositionArray.front() )
+	
+	for m in ListOfMaterials :
+		m.set_shader_parameter( "CircleCentres", PositionArray )
+		m.set_shader_parameter( "NumCircleCentres", PositionArray.size() )
