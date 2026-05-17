7a38e83cc1dfdc96b6fd4ed5aed64cbb56c90060
Kamil <kamil.borkow@wp.pl>
2026-05-17 22:09:08 +0200

    Dimension border smoothing

--- objects/dimensions/dimension_split.gd
+++ objects/dimensions/dimension_split.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 signal swap_dimensions
-#Occluder shold be static and area should be offset based on player direction
-var swap_distance := 12.0
+
+var swap_distance := 17.0
 var occluder_offset := 15.0
 var entered_from_top = false
 var border_width = 135.0
+var occluder_speed := 240.0
+var min_distance := 5.0
 @onready var lightOccluder = $LightOccluder2D
 @export var lightOccRestPos = 1296.0
 @onready var area = $Area2D
@@ -16,19 +18,23 @@ func _on_area_2d_body_entered(body):
 		entered_from_top = body.global_position.y < area.global_position.y
 
 
-func _process(_delta):
+func _process(delta):
 	var overlapping_bodies = area.get_overlapping_bodies()
 	var player = get_tree().get_first_node_in_group("player")
 	
 	#moving dimension border for better visibility
+	var target_y = lightOccRestPos
 	if abs(player.global_position.y - lightOccRestPos) <= swap_distance:
 		if abs(player.global_position.x - lightOccluder.global_position.x) < border_width:
 			if player.global_position.y < lightOccRestPos:
-				lightOccluder.global_position.y = lightOccRestPos + occluder_offset
+				target_y = lightOccRestPos + occluder_offset
+				target_y = max(target_y, player.global_position.y + min_distance)
 			else:
-				lightOccluder.global_position.y = lightOccRestPos - occluder_offset
+				target_y = lightOccRestPos - occluder_offset
+				target_y = min(target_y, player.global_position.y - min_distance)
 	else:
-		lightOccluder.global_position.y = lightOccRestPos
+		target_y = lightOccRestPos
+	lightOccluder.global_position.y = move_toward(lightOccluder.global_position.y, target_y, occluder_speed * delta)
 	
 	#swaping dimensions
 	for body in overlapping_bodies:
